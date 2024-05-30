84f3f76411e1a4185d7fa6a6d9fd484b757a62c6
DayoungLee <yooung513@gmail.com>
2024-05-30 21:37:16 +0900

    240530 Update MainApplication 위치 및 이름 변경

--- src/main/java/coffee/kiosk/MainApplication.java
+++ src/main/java/coffee/kiosk/MainApplication.java
@@ -1,12 +1,11 @@
-package coffee.kiosk.Controller;
+package coffee.kiosk;
 
-import coffee.kiosk.HelloApplication;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainController extends Application {
+public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
