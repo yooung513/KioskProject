f57a4c853016d3cdf52cc1c1778539e58eb9bc88
DayoungLee <yooung513@gmail.com>
2024-05-31 11:05:50 +0900

    240531 Create 메뉴관리 생성

---
+++ src/main/java/coffee/kiosk/Controller/ManagementController.java
@@ -0,0 +1,35 @@
+package coffee.kiosk.Controller;
+
+
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ManagementController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    public void goTotal(MouseEvent e) {
+        System.out.println("goTotal");
+    }
+
+    public void goCoffee(MouseEvent e) {
+    }
+
+    public void goBeverage(MouseEvent mouseEvent) {
+    }
+
+    public void goSmoothie(MouseEvent mouseEvent) {
+    }
+
+    public void goTea(MouseEvent mouseEvent) {
+    }
+
+    public void goDessert(MouseEvent mouseEvent) {
+    }
+
+}
