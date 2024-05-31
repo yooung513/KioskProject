4cee07eb48c45329688828b9fb65e54d59a22b39
DayoungLee <yooung513@gmail.com>
2024-05-31 17:32:42 +0900

    240531 Create DB Connection 생성

---
+++ src/main/java/coffee/kiosk/DAO/DBConnection.java
@@ -0,0 +1,27 @@
+package coffee.kiosk.DAO;
+
+import java.sql.*;
+
+public class DBConnection {
+    public static Connection con;
+
+    public static Connection getConnection() {
+        try {
+
+            Class.forName("oracle.jdbc.OracleDriver");
+
+            String url = "jdbc:oracle:thin:@localhost:1521:xe";
+            String id = "dev_kiosk";
+            String pw = "1234";
+
+            con = DriverManager.getConnection(url, id, pw);
+
+            System.out.println("DB 연결 성공");
+
+        } catch (Exception e) {
+            System.out.println("DB 연결 실패");
+        }
+
+        return con;
+    }
+}
