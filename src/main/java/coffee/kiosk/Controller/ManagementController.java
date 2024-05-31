b7897d1247d54070731aaea3ea47b851fe2689a0
DayoungLee <yooung513@gmail.com>
2024-05-31 12:04:28 +0900

    240531 Update 주석 추가

--- src/main/java/coffee/kiosk/Controller/ManagementController.java
+++ src/main/java/coffee/kiosk/Controller/ManagementController.java
@@ -1,11 +1,22 @@
 package coffee.kiosk.Controller;
 
 
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+
+
+/**
+ *  Date    : 2024.05.31
+ *  Author  : 이다영
+ *  Summary : 메뉴 관리 화면 컨트롤러
+ */
 
 public class ManagementController implements Initializable {
 
@@ -32,4 +43,12 @@ public class ManagementController implements Initializable {
     public void goDessert(MouseEvent mouseEvent) {
     }
 
+    public void goHome(MouseEvent mouseEvent) {
+        FXMLLoader managementLoader = new FXMLLoader(getClass().getResource("/coffee/kiosk/management.fxml"));
+        Scene managementScene = new Scene(managementLoader.load());
+        Button managementBtn = (Button) scene.lookup("#management");
+        managementBtn.setOnAction(event -> {
+            stage.setScene(managementScene);
+        });
+    }
 }
