abeeb10faa01be61aa18bef4b75c32b43b766711
jaehyuk <dhwogur0@gmail.com>
2024-05-31 10:51:24 +0900

    240531 Update home,Mainapplicaiton 씬 변경 기능 추가

--- src/main/java/coffee/kiosk/MainApplication.java
+++ src/main/java/coffee/kiosk/MainApplication.java
@@ -3,6 +3,7 @@ package coffee.kiosk;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
@@ -12,6 +13,16 @@ public class MainApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("home.fxml"));
         System.setProperty("prism.lcdtext", "false");
         Scene scene = new Scene(fxmlLoader.load(), 500, 800);
+
+        // 개점 처리
+        // home scene -> cafemenu scene move
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/coffee/kiosk/cafemenu.fxml"));
+        Scene cafemenuScene = new Scene(loader.load());
+        Button button = (Button) scene.lookup("#openstore");
+        button.setOnAction(event -> {
+            stage.setScene(cafemenuScene);
+        });
+
         stage.setTitle("Welcome To JH Cafe");
         stage.setScene(scene);
         stage.show();
